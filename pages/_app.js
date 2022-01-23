b271dc438bfa41a15a996281591af4d58569ac92
Halil Koca <ihalilkoca@gmail.com>
2022-01-24 00:10:35 +0300

    changed APP.JS and index.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,29 @@
 import '../styles/globals.css'
+import Link from 'next/link'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function KryptoBirdMarketplace({ Component, pageProps }) {
+  return (
+    <div>
+      <nav className='border-b p-6' style={{ backgorundColor: 'purple' }}>
+        <p className='text-4x1 font-bold text-white'>KryptoBird Marketplace</p>
+        <div className='flex mt-4 justify-center'>
+          <Link href='/'>
+            <a className='mr-4'>Main Marketplace</a>
+          </Link>
+          <Link href='/mint-item'>
+            <a className='mr-4'>Mint Tokens</a>
+          </Link>
+          <Link href='/my-nfts'>
+            <a className='mr-4'>My NFTs</a>
+          </Link>
+          <Link href='/accout-dashboard'>
+            <a className='mr-4'>Account Dashboard</a>
+          </Link>
+          <Component {...pageProps} />
+        </div>
+      </nav>
+    </div>
+  )
 }
 
-export default MyApp
+export default KryptoBirdMarketplace;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 module.exports = {
-  content: [],
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
